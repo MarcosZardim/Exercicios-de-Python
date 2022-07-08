514b349e40f513cbb1f7d590ee95f16bff1f04d5
Marcos Zardim <107150029+MarcosZardim@users.noreply.github.com>
2022-07-08 20:13:05 -0300

    Delete ex090.py

--- ex090.py
+++
@@ -1,8 +0,0 @@
-aluno = {'Nome': str(input('Nome: ')).title()}
-aluno['Média'] = float(input(f'Média de {aluno["Nome"]}: '))
-if aluno['Média'] >= 7.0:
-    aluno['Situação'] = 'Aprovado'
-else:
-    aluno['Situação'] = 'Reprovado'
-for k, v in aluno.items():
-    print(f'{k} é igual a {v}')
