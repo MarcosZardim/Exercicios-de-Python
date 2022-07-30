824c4e9b633a5f27c6ae8566e7f593384423e6d2
Marcos Eduardo Trancoso Zardim <107150029+MarcosZardim@users.noreply.github.com>
2022-07-30 14:37:49 -0300

    Exercícios de Python

---
+++ ex107/moeda.py
@@ -0,0 +1,15 @@
+def aumentar(n, v):
+    return ((v / 100) * n) + n
+
+
+def diminuir(n, v):
+    return (-(v / 100) * n) + n
+
+
+
+def dobro(n):
+    return n * 2
+
+
+def metade(n):
+    return n / 2
